dabb8f58614479c99c02010c5c63bd6f0e70a998
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-11-04 09:49:49 -0400

    query account balance and more staking info

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,11 +9,81 @@ export type ExtensionState = {
   error: null | Error
 }
 
+export type AccountDetails = {
+  nonce: number
+  consumers: number
+  providers: number
+  sufficients: number
+  data: {
+    free: number | bigint
+    reserved: number | bigint
+    frozen: number | bigint
+    flags: string
+  }
+}
+
 export type StakingConstants = {
   maxNominators: number
   minOperatorStake: bigint
   stakeEpochDuration: number
   stakeWithdrawalLockingPeriod: number
+  domainRegistry: DomainRegistry[]
+  domainStakingSummary: DomainStakingSummary[]
+  operatorIdOwner: OperatorIdOwner[]
+  operators: Operators[]
+  pendingStakingOperationCount: PendingStakingOperationCount[]
+}
+
+export type DomainRegistry = {
+  ownerAccountId: string
+  createdAt: number
+  genesisReceiptHash: string
+  domainConfig: {
+    domainName: string
+    runtimeId: number
+    maxBlockSize: number
+    maxBlockWeight: {
+      refTime: number
+      proofSize: string
+    }
+    bundleSlotProbability: number[]
+    targetBundlesPerBlock: number
+    operatorAllowList: {
+      operators: string[]
+    }
+  }
+}
+
+export type DomainStakingSummary = {
+  currentEpochIndex: number
+  currentTotalStake: string
+  currentOperators: {
+    [key: string]: string
+  }
+  nextOperators: string[]
+  currentEpochRewards: {
+    [key: string]: string
+  }
+}
+
+export type OperatorIdOwner = {
+  [key: string]: string
+}
+
+export type Operators = {
+  signingKey: string
+  currentDomainId: number
+  nextDomainId: number
+  minimumNominatorStake: string
+  nominationTax: number
+  currentTotalStake: string
+  currentEpochRewards: number
+  totalShares: number
+  status: string
+}
+
+export type PendingStakingOperationCount = {
+  [key: string]: number
 }
 
 export type Registration = {
