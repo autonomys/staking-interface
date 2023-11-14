1f9c2236ac2b3732405414676b0f1f35b8220b47
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-11-14 10:34:48 -0500

    add more console log on tx

--- src/hooks/useTx.ts
+++ src/hooks/useTx.ts
@@ -51,6 +51,12 @@ export const useTx = () => {
           .signAndSend(extension.data?.defaultAccount.address, { signer: injectedExtension.signer }, ({ status }) => {
             console.log('status', status)
           })
+          .then((result) => {
+            console.log('result', result)
+          })
+          .catch((error) => {
+            console.log('error', error)
+          })
         console.log('result', result)
       }
       return
@@ -73,6 +79,12 @@ export const useTx = () => {
           .signAndSend(extension.data?.defaultAccount.address, { signer: injectedExtension.signer }, ({ status }) => {
             console.log('status', status)
           })
+          .then((result) => {
+            console.log('result', result)
+          })
+          .catch((error) => {
+            console.log('error', error)
+          })
         console.log('result', result)
       }
       return
@@ -94,6 +106,12 @@ export const useTx = () => {
           .withdrawStake(operatorId, hexAmount)
           .signAndSend(extension.data?.defaultAccount.address, { signer: injectedExtension.signer }, ({ status }) => {
             console.log('status', status)
+          })
+          .then((result) => {
+            console.log('result', result)
+          })
+          .catch((error) => {
+            console.log('error', error)
           })
         console.log('result', result)
       }
