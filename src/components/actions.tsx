0fa0c790bd497e73ea4707ba60042648f645e0b6
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-11-22 07:14:59 -0500

    improve modal style

--- src/components/actions.tsx
+++ src/components/actions.tsx
@@ -1,7 +1,6 @@
 import { ChevronDownIcon } from '@chakra-ui/icons'
 import {
   Button,
-  Center,
   FormControl,
   FormErrorMessage,
   FormHelperText,
@@ -29,7 +28,7 @@ import { ActionType, ROUTES, SYMBOL, actionButtonStyles } from '../constants'
 import { useManage } from '../hooks/useManage'
 import { useExtension } from '../states/extension'
 import { useRegistration } from '../states/registration'
-import { capitalizeFirstLetter, formatAddress } from '../utils'
+import { capitalizeFirstLetter } from '../utils'
 
 interface ActionsProps {
   operatorId: string
@@ -95,80 +94,77 @@ export const Actions: React.FC<ActionsProps> = ({ operatorId }) => {
         </MenuList>
       </Menu>
 
-      <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={handleCloseModal}>
+      <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={handleCloseModal} size='xl'>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>
             {actionSelected != null && capitalizeFirstLetter(actionSelected)} on Operator Id #{operatorId}
           </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            <Center>
-              <VStack>
-                <Text>Operator Account {operator && operator.operatorOwner}</Text>
-                <Text>
-                  Signing Key{' '}
-                  {operator && (
-                    <Link href={`${ROUTES.OPERATOR_STATS}/${operator.operatorDetail.signingKey}`}>
-                      {formatAddress(operator.operatorDetail.signingKey)}
-                    </Link>
+          <ModalBody p={4}>
+            <VStack w='100%' align='left'>
+              <Text fontWeight='500'>Operator Account</Text>
+              <Text>{operator && operator.operatorOwner}</Text>
+              <Text fontWeight='500'>Signing Key</Text>
+              <Text fontSize='12px'>
+                {operator && (
+                  <Link href={`${ROUTES.OPERATOR_STATS}/${operator.operatorDetail.signingKey}`}>
+                    {operator.operatorDetail.signingKey}
+                  </Link>
+                )}
+              </Text>
+              {actionSelected === ActionType.Deregister && <Text>Do you really want to de-register as operator?</Text>}
+              {actionSelected === ActionType.AddFunds && (
+                <FormControl isInvalid={isErrorsField['amount']}>
+                  <InputGroup size='md' mt='4' w='100%'>
+                    <Input
+                      name='amount'
+                      borderColor='brand.500'
+                      border='1px'
+                      placeholder={`Amount, ${SYMBOL}`}
+                      value={addFundsAmount.formattedAmount}
+                      onChange={(e) => handleChangeAmount(ActionType.AddFunds, e)}
+                      _placeholder={{ color: '#7D7D7D' }}
+                    />
+                    <InputRightElement>
+                      <Button m={1} onClick={handleMaxAmountToAddFunds} size='sm'>
+                        Max
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                  {isErrorsField['amount'] ? (
+                    <FormErrorMessage h='10'>The amount you enter is not valid</FormErrorMessage>
+                  ) : (
+                    <FormHelperText h='10'></FormHelperText>
                   )}
-                </Text>
-                {actionSelected === ActionType.Deregister && (
-                  <Text>Do you really want to de-register as operator?</Text>
-                )}
-                {actionSelected === ActionType.AddFunds && (
-                  <FormControl isInvalid={isErrorsField['amount']}>
-                    <InputGroup size='md' mt='4' w='20vw'>
-                      <Input
-                        name='amount'
-                        borderColor='brand.500'
-                        border='1px'
-                        placeholder={`Amount, ${SYMBOL}`}
-                        value={addFundsAmount.formattedAmount}
-                        onChange={(e) => handleChangeAmount(ActionType.AddFunds, e)}
-                        _placeholder={{ color: '#7D7D7D' }}
-                      />
-                      <InputRightElement>
-                        <Button m={1} onClick={handleMaxAmountToAddFunds} size='sm'>
-                          Max
-                        </Button>
-                      </InputRightElement>
-                    </InputGroup>
-                    {isErrorsField['amount'] ? (
-                      <FormErrorMessage h='10'>The amount you enter is not valid</FormErrorMessage>
-                    ) : (
-                      <FormHelperText h='10'></FormHelperText>
-                    )}
-                  </FormControl>
-                )}
-                {actionSelected === ActionType.Withdraw && (
-                  <FormControl isInvalid={isErrorsField['amount']}>
-                    <InputGroup size='md' mt='4' w='20vw'>
-                      <Input
-                        name='amount'
-                        borderColor='brand.500'
-                        border='1px'
-                        placeholder={`Amount, ${SYMBOL}`}
-                        value={withdrawAmount.formattedAmount}
-                        onChange={(e) => handleChangeAmount(ActionType.Withdraw, e)}
-                        _placeholder={{ color: '#7D7D7D' }}
-                      />
-                      <InputRightElement>
-                        <Button m={1} onClick={handleMaxAmountToWithdraw} size='sm'>
-                          Max
-                        </Button>
-                      </InputRightElement>
-                    </InputGroup>
-                    {isErrorsField['amount'] ? (
-                      <FormErrorMessage h='10'>The amount you enter is not valid</FormErrorMessage>
-                    ) : (
-                      <FormHelperText h='10'></FormHelperText>
-                    )}
-                  </FormControl>
-                )}
-              </VStack>
-            </Center>
+                </FormControl>
+              )}
+              {actionSelected === ActionType.Withdraw && (
+                <FormControl isInvalid={isErrorsField['amount']}>
+                  <InputGroup size='md' mt='4' w='100%'>
+                    <Input
+                      name='amount'
+                      borderColor='brand.500'
+                      border='1px'
+                      placeholder={`Amount, ${SYMBOL}`}
+                      value={withdrawAmount.formattedAmount}
+                      onChange={(e) => handleChangeAmount(ActionType.Withdraw, e)}
+                      _placeholder={{ color: '#7D7D7D' }}
+                    />
+                    <InputRightElement>
+                      <Button m={1} onClick={handleMaxAmountToWithdraw} size='sm'>
+                        Max
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                  {isErrorsField['amount'] ? (
+                    <FormErrorMessage h='10'>The amount you enter is not valid</FormErrorMessage>
+                  ) : (
+                    <FormHelperText h='10'></FormHelperText>
+                  )}
+                </FormControl>
+              )}
+            </VStack>
           </ModalBody>
 
           <ModalFooter>
