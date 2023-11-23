571be5430170da4e0956dd7e14094452cb4a5cbe
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-11-23 06:31:15 -0500

    Format TSSC values #47

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,9 +2,12 @@ import { DECIMALS } from '../constants'
 
 export const formatAddress = (address: string) => `${address.slice(0, 4)}...${address.slice(-6)}`
 
-export const formatNumber = (number: number | string, decimals = 4) => {
+export const formatNumber = (number: number | string, decimals = 3) => {
   if (typeof number === 'string') number = number.includes('.') ? parseFloat(number) : parseInt(number)
-  return (Math.round(number * 100) / 100).toFixed(decimals).toLocaleString()
+  return (Math.round(number * 100) / 100)
+    .toFixed(decimals)
+    .toLocaleString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
 
 export const parseNumber = (number: string) => (parseFloat(number) * 10 ** DECIMALS).toString()
@@ -13,4 +16,4 @@ export const capitalizeFirstLetter = (string: string) => string.charAt(0).toUppe
 
 export const hexToNumber = (hex: string) => parseInt(hex, 16) / 10 ** DECIMALS
 
-export const hexToFormattedNumber = (hex: string, decimals = 4) => formatNumber(hexToNumber(hex), decimals)
+export const hexToFormattedNumber = (hex: string, decimals = 3) => formatNumber(hexToNumber(hex), decimals)
