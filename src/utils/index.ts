9f88641f63e72ec573532f7e783a6ae61bca1135
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-11-24 07:18:39 -0500

    add chain data to state management with type and init values

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,10 +7,11 @@ export const formatNumber = (number: number | string, decimals = 4) => {
   return (Math.round(number * 100) / 100).toFixed(decimals).toLocaleString()
 }
 
-export const parseNumber = (number: string) => (parseFloat(number) * 10 ** DECIMALS).toString()
+export const parseNumber = (number: string, decimals: number = DECIMALS) =>
+  (parseFloat(number) * 10 ** decimals).toString()
 
 export const capitalizeFirstLetter = (string: string) => string.charAt(0).toUpperCase() + string.slice(1)
 
-export const hexToNumber = (hex: string) => parseInt(hex, 16) / 10 ** DECIMALS
+export const hexToNumber = (hex: string, decimals: number = DECIMALS) => parseInt(hex, 16) / 10 ** decimals
 
 export const hexToFormattedNumber = (hex: string, decimals = 4) => formatNumber(hexToNumber(hex), decimals)
