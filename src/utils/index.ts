b0d3c5ef0fc3a6d7f06c2c11527068b5ae35c239
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-11-25 19:15:04 -0500

    remove 2 decimals rounding

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,13 +4,14 @@ export const formatAddress = (address: string) => `${address.slice(0, 4)}...${ad
 
 export const formatNumber = (number: number | string, decimals = 3) => {
   if (typeof number === 'string') number = number.includes('.') ? parseFloat(number) : parseInt(number)
-  return (Math.round(number * 100) / 100)
+  return number
     .toFixed(decimals)
     .toLocaleString()
     .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
 
-export const parseNumber = (number: string, decimals: number = DECIMALS) => (BigInt(parseFloat(number)) * BigInt(10 ** decimals)).toString()
+export const parseNumber = (number: string, decimals: number = DECIMALS) =>
+  (BigInt(parseFloat(number)) * BigInt(10 ** decimals)).toString()
 
 export const capitalizeFirstLetter = (string: string) => string.charAt(0).toUpperCase() + string.slice(1)
 
