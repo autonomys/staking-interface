7f131ffed87c3b75a0248c431fb0dad7a015a4bc
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-12-01 19:27:20 -0500

    add new views in stats page

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -1,18 +1,23 @@
 import { Box, HStack, Heading } from '@chakra-ui/react'
 import React, { useEffect } from 'react'
 import { Wallet } from '../components/icons'
+import { OperatorsCards } from '../components/operatorsCards'
 import { OperatorsList } from '../components/operatorsList'
 import { OperatorsTotal } from '../components/operatorsTotal'
-import { headingStyles, pageStyles } from '../constants'
+import { ViewSelector } from '../components/viewSelector'
+import { OperatorListType, headingStyles, pageStyles } from '../constants'
 import { useOnchainData } from '../hooks/useOnchainData'
+import { useView } from '../states/view'
 
 const Page: React.FC = () => {
   const { handleOnchainData } = useOnchainData()
+  const { operatorsListType } = useView()
 
   useEffect(() => {
     handleOnchainData()
   }, [handleOnchainData])
 
+  console.log('operatorsListType', operatorsListType)
   return (
     <Box {...pageStyles}>
       <HStack>
@@ -20,7 +25,9 @@ const Page: React.FC = () => {
         <Heading {...headingStyles.page}>Stats</Heading>
       </HStack>
       <OperatorsTotal />
-      <OperatorsList />
+      <ViewSelector />
+      {operatorsListType === OperatorListType.CARD_GRID && <OperatorsCards />}
+      {operatorsListType === OperatorListType.LIST && <OperatorsList />}
     </Box>
   )
 }
