e6adef10fe277a434062a84563f330b29e899c70
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-12-09 11:39:44 -0500

    add small pie chart to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Heading, Text } from '@chakra-ui/react'
+import { Box, Grid, GridItem, Heading, Text } from '@chakra-ui/react'
+import dynamic from 'next/dynamic'
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useEffect } from 'react'
@@ -8,6 +9,8 @@ import { EXTERNAL_ROUTES, ROUTES, headingStyles, pageStyles, textStyles } from '
 import { useOnchainData } from '../hooks/useOnchainData'
 import { useWallet } from '../hooks/useWallet'
 import { useExtension } from '../states/extension'
+
+const PieGraph = dynamic(() => import('../components/pieGraph').then((m) => m.PieGraph), { ssr: false })
 
 const Page: React.FC = () => {
   const subspaceAccount = useExtension((state) => state.subspaceAccount)
@@ -22,12 +25,19 @@ const Page: React.FC = () => {
     <Box {...pageStyles}>
       <Intro />
       <Box mt='66px'>
-        <Heading {...headingStyles.page}>Setup a node</Heading>
-        <Link href={EXTERNAL_ROUTES.OPERATORS_DOCS} target='_blank'>
-          <Text {...textStyles.link}>Please follow the docs to setup a node</Text>
-        </Link>
         <Box mt='4'>
-          <Image src='/images/SetupANode.png' width='561' height='326' alt='Setup a Node Readme' />
+          <Grid templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)']} gap={[2, 4, 6]}>
+            <GridItem>
+              <Heading {...headingStyles.page}>Setup a node</Heading>
+              <Link href={EXTERNAL_ROUTES.OPERATORS_DOCS} target='_blank'>
+                <Text {...textStyles.link}>Please follow the docs to setup a node</Text>
+              </Link>
+              <Image src='/images/SetupANode.png' width='561' height='326' alt='Setup a Node Readme' />
+            </GridItem>
+            <GridItem>
+              <PieGraph small />
+            </GridItem>
+          </Grid>
         </Box>
 
         {extension.data ? (
